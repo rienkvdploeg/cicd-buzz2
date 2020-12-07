3feec050ebf0dc4a10cbcb45e917e9247284d2c1
rienkvdploeg <rienk.vanderploeg@hu.nl>
2020-12-07 17:47:46 +0100

    again and again added

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
-    page += "Greetings from Rienk again</br>"
+    page += "Greetings from Rienk again and again!</br>"
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
